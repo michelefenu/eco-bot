280d7e39f8e45957a0a76a43a14daeb595593e90
Michele Fenu <michelefenu@gmail.com>
2019-07-11 13:18:00 +0200

    Version 0.2.0

--- src/bot.js
+++ src/bot.js
@@ -6,14 +6,6 @@ const moment = require('moment');
 const tz = require('moment-timezone');
 const utils = require('./utils');
 
-let municipalityData = "";
-
-async function getMunicipalityData() {
-    municipalityData = await service.getMunicipalityData(process.env.CODICE_COMUNE);
-}
-
-getMunicipalityData();
-
 const bot = new TelegramBotClient(
     process.env.BOT_TOKEN,
     {
@@ -25,33 +17,30 @@ const bot = new TelegramBotClient(
 bot.onText(/\/start/, (msg, match) => {
     const chatId = msg.chat.id;
 
+    let cityData = service.getCityData();
+
     bot.sendMessage(
         chatId,
-        `Ciao, sono EcoBot 👋\n\nTi posso dare informazioni sulla raccolta differenziata nel comune di *${municipalityData.Name}*\n\nProva a scrivere /domani per sapere che materiale verrà ritirato domani o /calendario per avere il calendario della raccolta per i prossimi sette giorni\n\nSono un bot *non* ufficiale e *non* collegato in alcun modo al comune di ${municipalityData.Name} o alla società che gestisce la raccolta differenziata, che non possono essere ritenuti responsabili per i contenuti delle nostre conversazioni\n\nAllora, che ti interessa sapere?`,
+        `Ciao, sono EcoBot 👋\n\nTi posso dare informazioni sulla raccolta differenziata nel comune di *${cityData.Name}*\n\nProva a scrivere /domani per sapere che materiale verrà ritirato domani o /calendario per avere il calendario della raccolta per i prossimi sette giorni\n\nSono un bot *non* ufficiale e *non* collegato in alcun modo al comune di ${cityData.Name} o alla società che gestisce la raccolta differenziata, che non possono essere ritenuti responsabili per i contenuti delle nostre conversazioni\n\nAllora, che ti interessa sapere?`,
         { parse_mode: 'Markdown' }
     );
 });
 
 bot.onText(/domani/i, (msg, match) => {
-    const chatId = msg.chat.id;
-
-    let calendar = municipalityData.Calendar;
+    
+    let materials = service.getTomorrowSchedule().materials.join(', ') || 'Nessun ritiro previsto';
 
     let currentTime = moment().tz("Europe/Rome").locale('it');
     currentTime = currentTime.add(18, 'h');
-
-    let materiali = calendar[currentTime.format('YYYY')][currentTime.format('MM')][currentTime.format('DD')];
 
     let dayName = currentTime.format('dddd');
     let dayNumber = currentTime.format('D');
     let monthName = currentTime.format('MMMM');
 
-    let message = "";
-    if(materiali)
-        message = `${utils.capitalize(dayName)} ${dayNumber} ${monthName} verrà ritirato\n*${materiali}*\n\n👉 Ricordati di portare fuori i contenitori entro le 6 del mattino`;
-    else
-        message = `*Nessun ritiro previsto*`;
-        
+    let message = `${utils.capitalize(dayName)} ${dayNumber} ${monthName} verrà ritirato\n*${materials}*\n\n👉 Ricordati di portare fuori i contenitori entro le 6 del mattino`;
+
+    const chatId = msg.chat.id;
+
     bot.sendMessage(
         chatId,
         message,
@@ -60,30 +49,28 @@ bot.onText(/domani/i, (msg, match) => {
 });
 
 bot.onText(/calendario/i, (msg, match) => {
-    const chatId = msg.chat.id;
-    
-    let calendar = municipalityData.Calendar;
+    let calendar = service.getCalendar();
 
     let currentTime = moment().tz("Europe/Rome").locale('it');
 
-    let message = "";
-    for (let i = 0; i < 7; i++) {
-        let materiali = (calendar[currentTime.format('YYYY')][currentTime.format('MM')][currentTime.format('DD')] || "Nessun ritiro");
-        message += currentTime.format('D') + " " + currentTime.format('MMMM') + ": *" + materiali + "*\n";
-        currentTime = currentTime.add(1, 'd')
+    let message = ``;
+    for (let i = 0; i < calendar.length; i++) {
+        message += `${currentTime.add(1, 'd').format('D MMMM')}: *${calendar[i].materials.join(', ')  || 'Nessun ritiro'}*\n`
     }
+
+    const chatId = msg.chat.id;
 
     bot.sendMessage(
         chatId,
-        `Ecco il calendario per i prossimi 7 giorni\n\n${message}\n\n👉 Ricordati di portare fuori i contenitori entro le 6 del mattino del giorno di ritiro`,
+        `Ecco il calendario per i prossimi 7 giorni\n\n${message}\n👉 Ricordati di portare fuori i contenitori entro le 6 del mattino del giorno di ritiro`,
         { parse_mode: 'Markdown' }
     );
 });
 
 bot.onText(/ciao|buongiorno|salve|buonasera|buon giorno|buona sera/i, (msg, match) => {
-    const chatId = msg.chat.id;
-
     let saluto = match[0].replace(' ', '');
+
+    const chatId = msg.chat.id;
 
     bot.sendMessage(
         chatId,
@@ -92,12 +79,12 @@ bot.onText(/ciao|buongiorno|salve|buonasera|buon giorno|buona sera/i, (msg, matc
     );
 });
 
-bot.onText(/grazie/i, (msg, match) => {
+bot.onText(/grazie|ti ringrazio/i, (msg, match) => {
     const chatId = msg.chat.id;
 
     bot.sendMessage(
         chatId,
-        `Grazie a te! 😉`,
+        `È sempre un piacere! 😉`,
         { parse_mode: 'Markdown' }
     );
 });
@@ -112,6 +99,6 @@ bot.onText(/.+/, (msg, match) => {
     );
 });
 
-bot.on("polling_error", (err,match) => {
+bot.on("polling_error", (err, match) => {
     console.log(err);
 });
--- src/service.js
+++ src/service.js
@@ -1,32 +1,96 @@
 //module service.js
 
 const https = require("https");
+const moment = require('moment');
+const tz = require('moment-timezone');
 
-const getMunicipalityData = function(municipalityCode) {
-    
-    let url = "";
-    if(process.env.NODE_ENV !== 'Development')
-        url = `https://firebasestorage.googleapis.com/v0/b/eco-bot-data.appspot.com/o/${municipalityCode}.json?alt=media`;
-    else
-        url = `https://firebasestorage.googleapis.com/v0/b/eco-bot-data.appspot.com/o/${municipalityCode}-dev.json?alt=media`;
-  
-    return new Promise(resolve => {
-        https.get(url, res => {
-            res.setEncoding("utf8");
+let cityData = "";
+let url = "";
 
-            let body = "";
+if (process.env.NODE_ENV !== 'Development')
+    url = `https://firebasestorage.googleapis.com/v0/b/eco-bot-data.appspot.com/o/${process.env.CITY_CODE}.json?alt=media`;
+else
+    url = `https://firebasestorage.googleapis.com/v0/b/eco-bot-data.appspot.com/o/${process.env.CITY_CODE}-dev.json?alt=media`;
 
-            res.on("data", data => {
-                body += data;
-            });
+https.get(url, res => {
+    res.setEncoding("utf8");
 
-            res.on("end", () => {
-                resolve(JSON.parse(body));
-            });
-        });
-    })
+    let body = "";
+
+    res.on("data", data => {
+        body += data;
+    });
+
+    res.on("end", () => {
+        cityData = JSON.parse(body);
+    });
+});
+
+const getCityName = function () {
+    return cityData.Name;
+}
+
+const getCityCode = function () {
+    return cityData.Name;
+}
+
+const getTomorrowSchedule = function () {
+    return getScheduleForDayOffset();
+}
+
+const getWarnings = function() {
+    return cityData.Warnings;
+}
+
+/**
+ * Returns the material that will be collected aftert 6AM on the day offset specified day as a parameter
+ * 
+ * 0 = tomorrow, 1 = tomorrow + 1 day, 2 = tomorrow + 3 days...
+ * 
+ * Default: tomorrow
+ */
+const getScheduleForDayOffset = function (offset) {
+    offset = offset || 0;
+
+    let calendar = cityData.Calendar;
+
+    let currentTime = moment().tz("Europe/Rome").locale('it');
+    // We add 18 hours rather than 24 for tomorrow because the "day" starts at 6AM 
+    currentTime.add(18, 'h').add(offset, 'd');
+
+    let dayInfo = calendar[currentTime.format('YYYY')][currentTime.format('MM')][currentTime.format('DD')];
+
+    return dayInfo;
+}
+
+/**
+ * Returns the collection calendar for the next n days
+ * 
+ * Default: 7 days
+ */
+const getCalendar = function (offset) {
+    offset = offset || 7;
+
+    let currentTime = moment().tz("Europe/Rome").locale('it');
+
+    let calendarInfo = [];
+    for (let i = 0; i < offset; i++) {
+        calendarInfo.push(getScheduleForDayOffset(i));
+        currentTime.add(1, 'd');
+    }
+
+    return calendarInfo;
+}
+
+const getCityData = function () {
+    return cityData;
 }
 
 module.exports = {
-    getMunicipalityData: getMunicipalityData
+    getCityName,
+    getCityCode,
+    getCityData,
+    getTomorrowSchedule,
+    getCalendar,
+    getWarnings
 }
